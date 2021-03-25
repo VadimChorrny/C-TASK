cd1873fb2621bd23ed17a85bf49bd50a969ade66
VadimChorrny <chorrny228@gmail.com>
2021-03-25 18:21:12 +0200

    add new hometask

--- STD_Interfaces_Hometask/Client.cs
+++ STD_Interfaces_Hometask/Client.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 
@@ -10,36 +12,63 @@ namespace STD_Interfaces_Hometask
         public ushort CodeProduct;
         public string NameProduct;
         public uint Price;
-        enum TypeProduct { Phone, Laptop, Tablet };
+        public enum TypeProduct { Phone, Laptop, Tablet };
+        private TypeProduct type;
+        public Article(ushort code, string name, uint price, TypeProduct type)
+        {
+            this.CodeProduct = code;
+            this.NameProduct = name;
+            this.Price = price;
+            this.type = type;
+        }
     }
-    class Client
+    class Client : IComparable, IComparer<Client>
     {
         public ushort CodeClient { get; }
         public String ShortName { get; }
         public String Adress { get; }
-        public String Number
+        public int Number { get; set; }
+        public uint AmountProduct;
+        public Client(ushort code, string name, string adress, int number)
         {
-            get => Number;
-            set
-            {
-                string tmp = value.Remove(0, 1);
-                if (tmp.All(c => char.IsDigit(c)) && value[0] == '+')
-                {
-                    this.Number = value;
-                }
-                else
-                {
-                    throw new Exception("Client number dont correct!");
-                }
-            }
+            this.CodeClient = code;
+            this.ShortName = name;
+            this.Adress = adress;
+            this.Number = number;
         }
-        public uint AmountProduct { get; }
-    }
-    struct RequestItem
-    {
-        public Article Product;
-        public uint AmountProduct { get; }
+        public override string ToString()
+        {
+            return $"Code product - {CodeClient}\n" +
+                $"Short name - {ShortName}\n" +
+                $"Adress - {Adress}\n" +
+                $"Number - {Number}";
 
+        }
+
+        public int CompareTo(object obj)
+        {
+            return this.ShortName.CompareTo(((Client)(obj)).ShortName);
+        }
+
+        public int Compare([AllowNull] Client x, [AllowNull] Client y)
+        {
+            return x.AmountProduct.CompareTo(y.AmountProduct);
+        }
+    }
+    struct RequestItem : ICloneable
+    {
+        public Article article;
+        public uint AmountProduct { get; set; }
+        public RequestItem(Article article, uint amount)
+        {
+            this.article = article;
+            this.AmountProduct = amount;
+        }
+
+        public object Clone()
+        {
+            return new RequestItem(this.article,this.AmountProduct);
+        }
     }
     struct Request
     {
@@ -47,7 +76,33 @@ namespace STD_Interfaces_Hometask
         public Client client;
         public DateTime date;
         public List<RequestItem> OrderProduct;
-        public readonly uint Sum { get; }
+        public readonly uint Sum => Sum;
+        public Request(ushort code, DateTime data, Client client)
+        {
+            this.CodeProduct = code;
+            this.date = data;
+            this.client = client;
+            OrderProduct = new List<RequestItem>();
+        }
 
+        public void AddRequest(Article article, uint amount)
+        {
+            OrderProduct.Add(new RequestItem(article, amount));
+            client.AmountProduct += 1;
+            Console.WriteLine(client);
+        }
+        public void RemoveRequest(Article article, uint amount)
+        {
+            OrderProduct.Remove(new RequestItem(article, amount));
+            client.AmountProduct -= 1;
+            Console.WriteLine(client);
+        }
+        public void RenderRequest()
+        {
+            foreach (var item in OrderProduct)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
--- STD_Interfaces_Hometask/Program.cs
+++ STD_Interfaces_Hometask/Program.cs
@@ -6,7 +6,20 @@ namespace STD_Interfaces_Hometask
     {
         static void Main(string[] args)
         {
+            Client client = new Client(1234,"Bill Gates","USA,NewYork,Brighton Beach",3495543);
+            Request request = new Request(10,new DateTime(2021,03,25),client);
+            Article article = new Article(10,"IPad Pro",2800,Article.TypeProduct.Tablet);
+            request.AddRequest(article,12);
+            //request.AddRequest(new Article(5,"MacBook Air",35000,Article.TypeProduct.Laptop),12);
+            //request.AddRequest(new Article(15,"Iphone 11",19500,Article.TypeProduct.Phone),12);
+            Client clients = new Client(1235, "Elon Mask", "USA,Californiya", 13435231);
+            Article articles = new Article(1, "MacBook Pro", 88000, Article.TypeProduct.Laptop);
+            request.AddRequest(articles, 10);
+            Console.WriteLine(client);
+            Console.WriteLine(clients);
 
+            //Client client = new Client(100,"Vova","Ukraine",38094245);
+            //Console.WriteLine(client);
         }
     }
 }
