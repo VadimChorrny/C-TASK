7de60b9c3db705ba55109f0233f6c3192c5a4464
VadimChorrny <chorrny228@gmail.com>
2021-04-04 09:11:31 +0300

    add new task

---
+++ Queue_Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Queue_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Add(1);
+            q.Add(2);
+            q.Add(3);
+            q.Add(4);
+            q.Add(5);
+            q.Add(6);
+            q.Add(7);
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+            q.Extract();
+
+            Console.WriteLine("After remove");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"First element - {q.Top()}");
+
+            Console.WriteLine($"All size collection - {q.Size}");
+        }
+    }
+}
---
+++ Queue_Task/Queue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue_Task
+{
+    interface IQueue<T>
+    {
+        void Add(T elem);
+        T Extract();
+        T Top();
+        uint Size { get; }
+    }
+    class Node<T>
+    {
+        public T Data { get; set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public Node<T> Next { get; set; }
+    }
+    class Queue<T> : IEnumerable<T>, IQueue<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        public uint Size{ get; set; }
+
+        public void Add(T elem)
+        {
+            Node<T> node = new Node<T>(elem);
+            Node<T> tmpNode = tail;
+            tail = node;
+            if (Size == 0)
+            {
+                head = tail;
+            }
+            else
+            {
+                tmpNode.Next = tail;
+            }
+            Size++;
+        }
+
+        public T Extract()
+        {
+            if (Size == 0)
+            {
+                Console.WriteLine("error ! count = 0");
+            }
+            T output = head.Data;
+            head = head.Next;
+            Size--;
+            return output;
+        }
+
+
+        public T Top()
+        {
+            return head.Data;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> curr = head;
+            while (curr != null)
+            {
+                yield return curr.Data;
+                curr = curr.Next;
+            }
+        }
+    }
+}
